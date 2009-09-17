cbdd47eaf5a76c8f8ac52e432e1b18bac3df2300
mail.fabian@gmx.de <mail.fabian@gmx.de@9f4641f4-a39c-11de-a118-c7387b1e4f66>
2009-09-17 15:49:46 +0000

    First Commit

---
+++ trunk/dfasimjoensuu/code/src/dfasimulator/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dfasimulator;
+
+/**
+ *
+ * @author Fabian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Welcome to the DFA Simulator");
+    }
+
+}
