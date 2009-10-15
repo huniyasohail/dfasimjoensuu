37f77166445577478b1521633bb4a7ca33d06cfe
kai.w1986 <kai.w1986@9f4641f4-a39c-11de-a118-c7387b1e4f66>
2009-10-15 12:38:02 +0000

    website url in info window

--- dfasimjoensuu/code/src/gui/DFAInfoWin.java
+++ dfasimjoensuu/code/src/gui/DFAInfoWin.java
@@ -5,8 +5,12 @@
  */
 package gui;
 
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  *
@@ -21,7 +25,10 @@ public class DFAInfoWin extends javax.swing.JDialog {
     /**
      * Height of the window
      */
-    private static final int WINDOW_HEIGHT = 360;
+    private static final int WINDOW_HEIGHT = 330;
+
+    /** Project website URI */
+    String websiteUri = "http://code.google.com/p/dfasimjoensuu/";
 
     /** Creates new form DFAMainWIn */
     public DFAInfoWin() {
@@ -30,6 +37,7 @@ public class DFAInfoWin extends javax.swing.JDialog {
         this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         centreWindow(this);
         this.setModal(true);
+        this.labelWebsiteUri.setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
     /** This method is called from within the constructor to
@@ -51,6 +59,8 @@ public class DFAInfoWin extends javax.swing.JDialog {
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        labelWebsiteUri = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("DFA Simulator");
@@ -59,7 +69,7 @@ public class DFAInfoWin extends javax.swing.JDialog {
         setResizable(false);
         getContentPane().setLayout(null);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36));
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel2.setText("DFA Simulator");
         getContentPane().add(jLabel2);
         jLabel2.setBounds(30, 10, 410, 50);
@@ -67,7 +77,7 @@ public class DFAInfoWin extends javax.swing.JDialog {
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel3.setText("GNU Gernal Public License v2/v3");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(30, 210, 390, 17);
+        jLabel3.setBounds(30, 250, 200, 17);
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel5.setText("Kai Winnekens, Fabian Bürger");
@@ -83,7 +93,7 @@ public class DFAInfoWin extends javax.swing.JDialog {
         getContentPane().add(jButton1);
         jButton1.setBounds(420, 250, 90, 40);
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel7.setText("University of Joensuu, Finland");
         getContentPane().add(jLabel7);
         jLabel7.setBounds(30, 90, 440, 17);
@@ -101,9 +111,9 @@ public class DFAInfoWin extends javax.swing.JDialog {
         jLabel10.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel10.setText("Version 1.0, October 2009");
         getContentPane().add(jLabel10);
-        jLabel10.setBounds(30, 230, 370, 17);
+        jLabel10.setBounds(30, 270, 162, 17);
 
-        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 14));
+        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel11.setText("This program comes with ABSOLUTELY NO WARRANTY!");
         getContentPane().add(jLabel11);
         jLabel11.setBounds(30, 180, 440, 17);
@@ -113,6 +123,22 @@ public class DFAInfoWin extends javax.swing.JDialog {
         getContentPane().add(jLabel4);
         jLabel4.setBounds(30, 70, 410, 17);
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Visit the project website:");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(30, 210, 150, 17);
+
+        labelWebsiteUri.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        labelWebsiteUri.setText(this.websiteUri);
+        labelWebsiteUri.setToolTipText("Click here to visit the project website");
+        labelWebsiteUri.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                labelWebsiteUriMouseClicked(evt);
+            }
+        });
+        getContentPane().add(labelWebsiteUri);
+        labelWebsiteUri.setBounds(180, 210, 340, 17);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
@@ -121,6 +147,30 @@ public class DFAInfoWin extends javax.swing.JDialog {
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void labelWebsiteUriMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelWebsiteUriMouseClicked
+        try {
+            URI website = new URI(this.websiteUri);
+            openUriInBrowser(website);
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
+        }
+    }//GEN-LAST:event_labelWebsiteUriMouseClicked
+
+    /**
+     * Opens an URI in the system default browser.
+     * @param uri The URI to open.
+     */
+    private void openUriInBrowser(URI uri) {
+        if (uri != null) {
+            if (java.awt.Desktop.isDesktopSupported()) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(uri);
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
@@ -146,6 +196,7 @@ public class DFAInfoWin extends javax.swing.JDialog {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
@@ -155,5 +206,6 @@ public class DFAInfoWin extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel labelWebsiteUri;
     // End of variables declaration//GEN-END:variables
 }
